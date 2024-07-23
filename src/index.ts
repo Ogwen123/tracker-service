c3ab0d8b3222c3131e3775f4c294d4e469947ecd
Ogwen123 <owen.d.jones@btinternet.com>
2024-07-23 16:42:36 +0100

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+
+//@ts-ignore
+BigInt.prototype.toJSON = function () { return this.toString() }
+
+dotenv.config()
+
+const app = express()
+const port = 3003
+
+//app.use(express.json())
+app.use(bodyParser.json())
+
+app.use('/*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PATCH,DELETE,OPTIONS")
+    res.header("Access-Control-Max-Age", "86400")
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`auth service loaded, ${port}`)
+})
